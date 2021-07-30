b89a0eeeb9bea671fe04a2a8c804e5078bc4924e
Gustavo Cabral Ishizava <gustavoishizava@hotmail.com>
2021-07-29 21:11:59 -0300

    Adicionando repositórios

---
+++ src/Services/Transactions/MBD.Transactions.Domain/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MBD.Core.Data;
+using MBD.Transactions.Domain.Entities;
+using MBD.Transactions.Domain.Enumerations;
+
+namespace MBD.Transactions.Domain.Interfaces.Repositories
+{
+    public interface ICategoryRepository : IBaseRepository<Category>
+    {
+        Task<IEnumerable<Category>> GetByTypeAsync(TransactionType type);
+    }
+}
---
+++ src/Services/Transactions/MBD.Transactions.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MBD.Transactions.Domain.Entities;
+using MBD.Transactions.Domain.Enumerations;
+using MBD.Transactions.Domain.Interfaces.Repositories;
+using MBD.Transactions.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MBD.Transactions.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly TransactionContext _context;
+
+        public CategoryRepository(TransactionContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Category category)
+        {
+            _context.Add(category);
+        }
+
+        public async Task<Category> GetByIdAsync(Guid id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetByTypeAsync(TransactionType type)
+        {
+            return await _context.Categories
+                    .AsNoTracking()
+                    .Where(x => x.Type == type)
+                    .ToListAsync();
+        }
+
+        public void Remove(Category category)
+        {
+            _context.Remove(category);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Update(category);
+        }
+    }
+}

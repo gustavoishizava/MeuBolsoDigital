c1df981b06d90defbc8c194efdc979a49fa42200
Gustavo Cabral Ishizava <gustavoishizava@hotmail.com>
2021-08-04 20:56:19 -0300

    Removendo usings

--- src/Services/Transactions/MBD.Transactions.API/Configuration/ApiConfiguration.cs
+++ src/Services/Transactions/MBD.Transactions.API/Configuration/ApiConfiguration.cs
@@ -1,5 +1,4 @@
 using System.Text.Json.Serialization;
-using MBD.Transactions.Domain.Enumerations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
--- tests/MBD.Transactions.Tests/unit tests/Entities/CategoryTests.cs
+++ tests/MBD.Transactions.Tests/unit tests/Entities/CategoryTests.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using System;
 using MBD.Core.DomainObjects;
 using MBD.Core.Enumerations;

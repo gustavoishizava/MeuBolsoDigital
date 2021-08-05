ed05b3e6d45e5e6e48ab35636d219b747c5a07d4
Gustavo Cabral Ishizava <gustavoishizava@hotmail.com>
2021-08-05 19:52:35 -0300

    Adicionando verificaçao de BankAccount e Category

--- src/Services/Transactions/MBD.Transactions.Application/Commands/TransactionCommandHandler.cs
+++ src/Services/Transactions/MBD.Transactions.Application/Commands/TransactionCommandHandler.cs
@@ -6,21 +6,26 @@ using MBD.Core.Identity;
 using MBD.Transactions.Application.Response;
 using MBD.Transactions.Domain.Entities;
 using MBD.Transactions.Domain.Interfaces.Repositories;
+using MBD.Transactions.Domain.Interfaces.Services;
 using MediatR;
 
 namespace MBD.Transactions.Application.Commands
 {
     public class TransactionCommandHandler : IRequestHandler<CreateTransactionCommand, IResult<TransactionResponse>>
     {
-        private readonly ITransactionRepository _repository;
+        private readonly ITransactionRepository _transactionRepository;
         private readonly IAspNetUser _aspNetUser;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IBankAccountService _bankAccountService;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public TransactionCommandHandler(ITransactionRepository repository, IAspNetUser aspNetUser, IUnitOfWork unitOfWork)
+        public TransactionCommandHandler(ITransactionRepository transactionRepository, IAspNetUser aspNetUser, IUnitOfWork unitOfWork, IBankAccountService bankAccountService, ICategoryRepository categoryRepository)
         {
-            _repository = repository;
+            _transactionRepository = transactionRepository;
             _aspNetUser = aspNetUser;
             _unitOfWork = unitOfWork;
+            _bankAccountService = bankAccountService;
+            _categoryRepository = categoryRepository;
         }
 
         public async Task<IResult<TransactionResponse>> Handle(CreateTransactionCommand request, CancellationToken cancellationToken)
@@ -29,17 +34,25 @@ namespace MBD.Transactions.Application.Commands
             if (!validation.IsValid)
                 return Result<TransactionResponse>.Fail();
 
+            var bankAccount = await _bankAccountService.GetByIdAsync(request.BankAccountId);
+            if (bankAccount == null)
+                return Result<TransactionResponse>.Fail("Conta bancária inválida.");
+
+            var category = await _categoryRepository.GetByIdAsync(request.CategoryId);
+            if (category == null)
+                return Result<TransactionResponse>.Fail("Categoria inválida.");
+
             var transaction = new Transaction(
                 _aspNetUser.UserId,
-                request.BankAccountId,
-                request.CategoryId,
+                bankAccount.Id,
+                category.Id,
                 request.ReferenceDate,
                 request.DueDate,
                 request.Value,
                 request.Description
             );
 
-            _repository.Add(transaction);
+            _transactionRepository.Add(transaction);
             await _unitOfWork.SaveChangesAsync();
 
             return Result<TransactionResponse>.Success(new TransactionResponse(transaction));

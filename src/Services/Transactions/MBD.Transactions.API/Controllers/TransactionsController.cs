36dd944172e98a5e960a967567c10ddfc313d1f7
Gustavo Cabral Ishizava <gustavoishizava@hotmail.com>
2021-08-07 12:23:29 -0300

    Adicionando endpoints de consulta

--- src/Services/Transactions/MBD.Transactions.API/Controllers/TransactionsController.cs
+++ src/Services/Transactions/MBD.Transactions.API/Controllers/TransactionsController.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using MBD.Core.Extensions;
 using MBD.Transactions.API.Models;
 using MBD.Transactions.Application.Commands;
+using MBD.Transactions.Application.Queries;
 using MBD.Transactions.Application.Response;
+using MBD.Transactions.Application.Response.Models;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -17,10 +22,12 @@ namespace MBD.Transactions.API.Controllers
     public class TransactionsController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ITransactionQuery _query;
 
-        public TransactionsController(IMediator mediator)
+        public TransactionsController(IMediator mediator, ITransactionQuery query)
         {
             _mediator = mediator;
+            _query = query;
         }
 
         [HttpPost]
@@ -47,6 +54,30 @@ namespace MBD.Transactions.API.Controllers
                 return BadRequest(new ErrorModel(result));
 
             return NoContent();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<TransactionModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _query.GetAllAsync();
+            if (result.IsNullOrEmpty())
+                return NoContent();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:GUID}")]
+        [ProducesResponseType(typeof(TransactionModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var result = await _query.GetByIdAsync(id);
+            if (!result.Succeeded)
+                return NotFound();
+
+            return Ok(result.Data);
         }
     }
 }

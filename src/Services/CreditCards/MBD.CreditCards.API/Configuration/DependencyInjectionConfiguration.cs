a2d4df18f1874794f1af415f819be56a0d655175
Gustavo Cabral Ishizava <gustavoishizava@hotmail.com>
2021-11-30 21:34:38 -0300

    remove serviço de integração rest

--- src/Services/CreditCards/MBD.CreditCards.API/Configuration/DependencyInjectionConfiguration.cs
+++ src/Services/CreditCards/MBD.CreditCards.API/Configuration/DependencyInjectionConfiguration.cs
@@ -1,23 +1,18 @@
-using System;
 using System.Reflection;
 using MBD.Core.Data;
 using MBD.Core.Identity;
-using MBD.CreditCards.API.Configuration.HttpClient;
 using MBD.CreditCards.Application.BackgroundServices;
 using MBD.CreditCards.Application.IntegrationEvents.Events;
 using MBD.CreditCards.Application.IntegrationEvents.Handlers;
 using MBD.CreditCards.Application.Interfaces;
 using MBD.CreditCards.Application.Services;
 using MBD.CreditCards.Domain.Interfaces.Repositories;
-using MBD.CreditCards.Domain.Interfaces.Services;
 using MBD.CreditCards.Infrastructure;
 using MBD.CreditCards.Infrastructure.Repositories;
-using MBD.CreditCards.Infrastructure.Services;
 using MBD.MessageBus;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Polly;
 
 namespace MBD.CreditCards.API.Configuration
 {
@@ -27,7 +22,6 @@ namespace MBD.CreditCards.API.Configuration
         {
             services.AddAppServices()
                     .AddRepositories()
-                    .AddHttpClients(configuration)
                     .AddConsumers()
                     .AddMessageBus()
                     .AddConfigurations(configuration)
@@ -53,21 +47,6 @@ namespace MBD.CreditCards.API.Configuration
             services.AddScoped<ICreditCardRepository, CreditCardRepository>();
             services.AddScoped<IBankAccountRepository, BankAccountRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-            return services;
-        }
-
-        private static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
-
-            services.AddHttpClient<IBankAccountService, BankAccountService>(config =>
-            {
-                config.BaseAddress = new Uri(configuration["UrlServices:BankAccountService"]);
-            })
-            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-            .AddPolicyHandler(PollyRetryConfiguration.WaitToRetry())
-            .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             return services;
         }
